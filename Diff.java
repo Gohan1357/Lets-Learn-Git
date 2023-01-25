69996b412990f47c1cf31696aa8ba7da15ccf038
Aneesh Sharma <gunukrillin@gmail.com>
2023-01-25 20:36:04 +0530

    added message  in Sum.java

--- Diff.java
+++ Diff.java
@@ -1,8 +1,8 @@
-public class Diff
+public class Sum
 {
     public static void main(String [] args){
         int a=7;
         int b=6;
-        System.out.println(a-b);
+        System.out.println("the Sum is "+(a-b));
     }
 }
