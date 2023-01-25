41f644f36e13324a7f3cb03d0d2698ba0add17f8
Aneesh Sharma <gunukrillin@gmail.com>
2023-01-25 22:19:05 +0530

    added multiply for Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -3,6 +3,6 @@
     public static void main(String [] args){
         int a=7;
         int b=6;
-        System.out.println("the Sum is "+(a*b));
+        System.out.println("the Sum is the  "+(a*b));
     }
 }
